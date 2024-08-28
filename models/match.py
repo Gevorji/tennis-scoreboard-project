428927fab5909902da0c29b6444f2afcdc58019d
Gevorji <techno9882@gmail.com>
2024-08-28 20:42:19 +0300

    Add match score attr to match model

--- models/match.py
+++ models/match.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 
 from .basemodel import BaseModel
+from .jsonencodedtype import JSONEncodedStruct
 
 
 class Match(BaseModel):
@@ -11,6 +12,7 @@ class Match(BaseModel):
     player1_id: Mapped[int] = mapped_column(ForeignKey('player.player_id'))
     player2_id: Mapped[int] = mapped_column(ForeignKey('player.player_id'))
     winner_id: Mapped[int | None] = mapped_column(ForeignKey('player.player_id'))
+    match_score = mapped_column(JSONEncodedStruct)
 
     player1 = relationship('Player', foreign_keys=[player1_id])
     player2 = relationship('Player', foreign_keys=[player2_id])
