d9e3ad53b4d183b976ec0693b9f51dbb1ba18812
Gevorji <techno9882@gmail.com>
2024-08-28 18:00:53 +0300

    Fix sqlalchemy models

--- models/match.py
+++ models/match.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
+from sqlalchemy import ForeignKey
 
 from .basemodel import BaseModel
 
@@ -8,6 +8,10 @@ class Match(BaseModel):
     __tablename__ = 'match'
 
     match_id: Mapped[int] = mapped_column(primary_key=True)
-    player1: Mapped[str] = relationship('player')
-    player2: Mapped[str] = relationship('player')
-    winner: Mapped[str] = relationship('player')
+    player1_id: Mapped[int] = mapped_column(ForeignKey('player.player_id'))
+    player2_id: Mapped[int] = mapped_column(ForeignKey('player.player_id'))
+    winner_id: Mapped[int | None] = mapped_column(ForeignKey('player.player_id'))
+
+    player1 = relationship('Player', foreign_keys=[player1_id])
+    player2 = relationship('Player', foreign_keys=[player2_id])
+    winner = relationship('Player', foreign_keys=[winner_id])
--- models/player.py
+++ models/player.py
@@ -1,14 +1,18 @@
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import or_
 
+from typing import List
 
 from .basemodel import BaseModel
+from .match import Match
 
 
 class Player(BaseModel):
     __tablename__ = 'player'
 
     player_id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column()
-
+    name: Mapped[str]
+    matches: Mapped[List[Match]] = relationship(
+        Match, primaryjoin=or_(Match.player1_id == player_id, Match.player2_id == player_id), viewonly=True
+    )
 
