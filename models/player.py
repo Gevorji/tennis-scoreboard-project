eca6b1468d6d1a849840f08cc2b39c69431e1ac4
Gevorji <techno9882@gmail.com>
2024-09-01 21:39:09 +0300

    Add unique constraint to player orm model

--- models/player.py
+++ models/player.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import or_
+from sqlalchemy import or_, UniqueConstraint
 
 from typing import List
 
@@ -11,7 +11,7 @@ class Player(BaseModel):
     __tablename__ = 'player'
 
     player_id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
+    name: Mapped[str] = mapped_column(unique=True)
     matches: Mapped[List[Match]] = relationship(
         Match, primaryjoin=or_(Match.player1_id == player_id, Match.player2_id == player_id), viewonly=True
     )
